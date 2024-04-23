1c82530bf303be0f63a936d2a87af8a69b6412aa
zhuba-Ahhh <84793349+zhuba-Ahhh@users.noreply.github.com>
2024-04-23 08:29:49 +0000

    Deploying to gh-pages from @ zhuba-Ahhh/zhuba-tools@da052a8da33b96a40207a2b5997cb7a503a833b4 🚀

--- Bom/getBattery.d.ts
+++ Bom/getBattery.d.ts
@@ -1,8 +1,10 @@
-interface BatteryStatus {
+export interface BatteryStatus {
     charging: boolean;
     chargingTime: number;
     dischargingTime: number;
     level: number;
 }
+export interface BatterySupportingNavigator extends Navigator {
+    getBattery?: () => Promise<BatteryStatus>;
+}
 export declare const getBatteryStatus: () => Promise<BatteryStatus>;
-export {};
--- utils/convertToCamelCase.d.ts
+++ utils/convertToCamelCase.d.ts
@@ -2,5 +2,5 @@ type CamelCase<S extends string> = S extends `${infer P}_${infer Q}${infer R}` ?
 type ConvertToCamelCase<T> = T extends object ? {
     [K in keyof T as CamelCase<K & string>]: ConvertToCamelCase<T[K]>;
 } : T;
-export declare const convertToCamelCase: <T extends object>(obj: T) => ConvertToCamelCase<T>;
+export declare const convertToCamelCase: <T extends object>(obj: T | null) => ConvertToCamelCase<T>;
 export {};
